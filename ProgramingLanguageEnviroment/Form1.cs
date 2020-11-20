9ab19cfa68195e25f5a079163119d4fd95d9e9b1
Greg Z <gregorz499@gmail.com>
2020-11-20 16:57:43 +0000

    created if command with end if.

--- ProgramingLanguageEnviroment/Form1.cs
+++ ProgramingLanguageEnviroment/Form1.cs
@@ -24,6 +24,10 @@ namespace ProgramingLanguageEnviroment
         private DrawTo MyLine;
         private int varVal;
         private String variable;
+        private bool iloop = true;
+
+        //extract user input from textbox
+        public String line;
         public Form1()
         {
             InitializeComponent();
@@ -43,8 +47,8 @@ namespace ProgramingLanguageEnviroment
         {
             if (e.KeyCode == Keys.Enter)
             {
-                //extract user input from textbox
-                String line = inputBox.Text;
+
+                line = inputBox.Text;
                 //extract command from command line
                 String command = command_line.Text.Trim().ToLower();
                 commandList = command.Split(',',' ');
@@ -129,127 +133,193 @@ namespace ProgramingLanguageEnviroment
                 inputBox.LoadFile(loadFileBox.FileName, RichTextBoxStreamType.PlainText);
             }
         }
-        
+
         /// <summary>
         /// method drawShape handles running commands
         /// </summary>
         public void drawShape()
         {
-             if (commandList[0].Equals("drawto") == true) // drawto command
-             {
-                 if(commandList.Length != 3)// checks for incorrect amount of values
-                 {
-                     MessageBox.Show("Wrong amount of values.\nExample: drawTo 50,50", "Error",
-                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
-                 else
-                 {
-                     MyLine.DrawLine(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
-                 }
-             }
-             else if (commandList[0].Equals("rectangle") == true) // rectangle command 
-             {
-                 if(commandList.Length != 3)// checks for incorrect amount of values
-                 {
-                     MessageBox.Show("Wrong amount of values.\nExample: rectangle 50,50", "Error",
-                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
-                 else
-                 {
-                     MyRectangle.DrawSquare(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
-                 }
-                 
-             }
-             else if (commandList[0].Equals("circle") == true) // circle command 
-             {
-                 if(commandList.Length != 2)// checks for incorrect amount of values
-                 {
-                     MessageBox.Show("Wrong amount of values.\nExample: circle 50", "Error",
-                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
-                 else
-                 {
-                     MyCircle.DrawCircle(Int32.Parse(commandList[1]));
-                 }
+            if (iloop)
+            {
+                
+                if (commandList[0].Equals("if"))
+                {
+                    String value = commandList[3];
+                    String compVal = commandList[2];
+                    String varbVal = commandList[1];
+                    foreach (KeyValuePair<string, int> opp in Var.dict)
+                    {
+                        if (opp.Key.Equals(varbVal))
+                        {
+                            if (compVal.Equals("="))
+                            {
+                                if (opp.Value == int.Parse(value))
+                                {
+                                    iloop = true;
+                                }
+                                else
+                                {
+                                    iloop = false;
+                                }
+                            }
 
-                 
-             }
-             else if (commandList[0].Equals("moveto") == true) // moveto command 
-             {
-                 if(commandList.Length !=3 )// checks for incorrect amount of values
-                 {
-                     MessageBox.Show("Wrong amount of values.\nExample: moveto 50,50", "Error",
-                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
-                 else
-                 {
-                     MoveTo.moveTo(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
-                 }
+                            if (compVal.Equals(">"))
+                            {
+                                if (opp.Value > int.Parse(value))
+                                {
+                                    iloop = true;
+                                }
+                                else
+                                {
+                                    iloop = false;
+                                }
+                            }
 
-             }
-             else if (commandList[0].Equals("triangle") == true)// triangle command
-             {
-                 if(commandList.Length != 5)// checks for incorrect amount of values
-                 {
-                     MessageBox.Show("Wrong amount of values.\nExample: triangle 150,150,100,30", "Error",
-                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
-                 else
-                 {
-                     MyTriangle.DrawTriangle(Int32.Parse(commandList[1]),Int32.Parse(commandList[2]),Int32.Parse(commandList[3]), Int32.Parse(commandList[4]));
-                 }
-                 
-             }
-             else if (commandList[0].Equals("clear") == true)// clear command 
-             {
-                 ClearImage();
-                 canvas.Refresh();
-             }
-             else if(commandList[0].Equals("reset") == true)// reset command 
-             {
-                 ResetPos();
-                 canvas.Refresh();
-             }
-             else if (commandList[0].Equals("pen") == true) // change pen colour command 
-             {
-                 PenColor.colorPen(commandList[1]);
-             }
-             else if (commandList[0].Equals("fill_on"))// turn fill on 
-             {
-                 ShapeFill.filll(true);
-             }
-             else if (commandList[0].Equals("fill_off"))// turn fill off
-             {
-                 ShapeFill.filll(false);
-             }
-             else if (commandList[0].Equals("save"))// save command 
-             {
-                 SaveFiles();
-             }
-             else if (commandList[0].Equals("load"))//load command
-             {
-                 LoadFiles();
-             }
-             else if (commandList[1].Equals("="))
-             {
-                 if (commandList.Length > 2)
-                 {
-                     variable = commandList[0];
-                         varVal = int.Parse(commandList[2]);
-                         Var.setVal(commandList[0], varVal);
-                         Var.addVal();
-                         Var.printDictionary();
+                            if (compVal.Equals("<"))
+                            {
+                                if (opp.Value < int.Parse(value))
+                                {
+                                    iloop = true;
+                                }
+                                else
+                                {
+                                    iloop = false;
+                                }
 
-                 }
-             }
-             else
-             {
-                 // checks for invalid commands 
-                 var lineCount = inputBox.Lines.Count();
-                 
-                 MessageBox.Show("Invalid command on line: " + lineCount, "Error",
-                     MessageBoxButtons.OK, MessageBoxIcon.Error);
-             }
+                            }
+
+                        }
+                    }
+
+                }
+                else if (commandList[0].Equals("endif"))
+                {
+                    iloop = true;
+                }
+                else if (commandList[0].Equals("drawto") == true) // drawto command
+                {
+                    if (commandList.Length != 3) // checks for incorrect amount of values
+                    {
+                        MessageBox.Show("Wrong amount of values.\nExample: drawTo 50,50", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MyLine.DrawLine(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
+                    }
+                }
+                else if (commandList[0].Equals("rectangle") == true) // rectangle command 
+                {
+                    if (commandList.Length != 3) // checks for incorrect amount of values
+                    {
+                        MessageBox.Show("Wrong amount of values.\nExample: rectangle 50,50", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MyRectangle.DrawSquare(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
+                    }
+
+                }
+                else if (commandList[0].Equals("circle") == true) // circle command 
+                {
+                    if (commandList.Length != 2) // checks for incorrect amount of values
+                    {
+                        MessageBox.Show("Wrong amount of values.\nExample: circle 50", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MyCircle.DrawCircle(Int32.Parse(commandList[1]));
+                    }
+
+
+                }
+                else if (commandList[0].Equals("moveto") == true) // moveto command 
+                {
+                    if (commandList.Length != 3) // checks for incorrect amount of values
+                    {
+                        MessageBox.Show("Wrong amount of values.\nExample: moveto 50,50", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MoveTo.moveTo(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]));
+                    }
+
+                }
+                else if (commandList[0].Equals("triangle") == true) // triangle command
+                {
+                    if (commandList.Length != 5) // checks for incorrect amount of values
+                    {
+                        MessageBox.Show("Wrong amount of values.\nExample: triangle 150,150,100,30", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MyTriangle.DrawTriangle(Int32.Parse(commandList[1]), Int32.Parse(commandList[2]),
+                            Int32.Parse(commandList[3]), Int32.Parse(commandList[4]));
+                    }
+
+                }
+                else if (commandList[0].Equals("clear") == true) // clear command 
+                {
+                    ClearImage();
+                    canvas.Refresh();
+                }
+                else if (commandList[0].Equals("reset") == true) // reset command 
+                {
+                    ResetPos();
+                    canvas.Refresh();
+                }
+                else if (commandList[0].Equals("pen") == true) // change pen colour command 
+                {
+                    PenColor.colorPen(commandList[1]);
+                }
+                else if (commandList[0].Equals("fill_on")) // turn fill on 
+                {
+                    ShapeFill.filll(true);
+                }
+                else if (commandList[0].Equals("fill_off")) // turn fill off
+                {
+                    ShapeFill.filll(false);
+                }
+                else if (commandList[0].Equals("save")) // save command 
+                {
+                    SaveFiles();
+                }
+                else if (commandList[0].Equals("load")) //load command
+                {
+                    LoadFiles();
+                }
+                else if (commandList[1].Equals("="))
+                {
+                    if (commandList.Length > 2)
+                    {
+                        variable = commandList[0];
+                        varVal = int.Parse(commandList[2]);
+                        Var.setVal(commandList[0], varVal);
+                        Var.addVal();
+                        Var.printDictionary();
+
+                    }
+                }
+                else
+                {
+                    // checks for invalid commands 
+                    var lineCount = inputBox.Lines.Count();
+
+                    MessageBox.Show("Invalid command on line: " + lineCount, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                if (commandList[0].Equals("endif"))
+                {
+                    iloop = true;
+                }
+            }
         }
         
         /// <summary>
